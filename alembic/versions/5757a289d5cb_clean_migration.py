3391f7f4b318c9595e1a6e4d07477a79c8011825
Julio Rios <julio.rios.araujo@gmail.com>
2025-08-28 09:57:52 -0300

    feature: test

---
+++ alembic/versions/5757a289d5cb_clean_migration.py
@@ -0,0 +1,24 @@
+"""clean_migration
+
+Revision ID: 5757a289d5cb
+Revises: ddaaf2df7883
+Create Date: 2025-08-28 09:23:32.432889
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5757a289d5cb'
+down_revision = 'ddaaf2df7883'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
