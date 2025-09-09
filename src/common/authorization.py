1f73b8b8789027a3de308df6c4522ababe37cd3c
Julio Rios <julio.rios.araujo@gmail.com>
2025-09-09 15:11:24 -0300

    feature: removing unnused files

---
+++ src/common/authorization.py
@@ -0,0 +1,47 @@
+# Repositories
+# Services
+from common.conexao_banco import session
+from common.conexao_banco import get_session
+from common.custom_exception import CustomException
+from common.error_messages import (
+    USER, X_NOT_FOUND, USER_BELONGS_TO_DEACTIVATED_CUSTOMER
+)
+# Tables
+from models.schema_public import User
+# Libs
+from sqlalchemy import and_
+from flask import request, g
+import jwt
+
+
+def get_current_user():
+    jwt_encoded = request.headers['Authorization']
+    jwt_encoded = jwt_encoded.replace('Bearer ', '')
+    jwt_decoded = jwt.decode(jwt_encoded, options={"verify_signature": False})
+    username = ''
+    if 'cognito:username' in jwt_decoded:
+        username = jwt_decoded['cognito:username']
+    elif 'username' in jwt_decoded:
+        username = jwt_decoded['username']
+
+    user = session.query(User).filter(
+        and_(User.Ativo, not User.Excluido, User.Username == username)
+    ).first()
+
+    if not user:
+        raise CustomException(X_NOT_FOUND.format(USER + ': ' + username))
+    if not user.Cliente.Ativo or user.Cliente.Excluido:
+        raise CustomException(USER_BELONGS_TO_DEACTIVATED_CUSTOMER)
+
+    return user
+
+
+def authenticate(app):
+    user = get_current_user()
+    if not user:
+        raise CustomException(X_NOT_FOUND.format(USER + ': ' + user.Username))
+    if not user.Cliente.Ativo or user.Cliente.Excluido:
+        raise CustomException(USER_BELONGS_TO_DEACTIVATED_CUSTOMER)
+
+    g.user = user
+    return user
